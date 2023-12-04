e4e670cd235318c8cc857beed1285489aff974de
ÁNGELA GIJÓN FLORES <Angela.Gijon@alu.uclm.es>
2023-12-04 21:56:11 +0100

    Botón de descargar y modificación del diseño de la pantalla "ReproduccionVideos"

--- Multimedia/ReproduccionVideos.xaml.cs
+++ Multimedia/ReproduccionVideos.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -47,7 +50,7 @@ namespace Multimedia
             if (e.Parameter is Tuple<string, string> data)
             {
                 String nombreVideo = data.Item1;
-                String descripcionVideo = data.Item2;
+                txtDescripcion.Text = data.Item2;
 
                 iniciarVideo(nombreVideo);
 
@@ -68,6 +71,56 @@ namespace Multimedia
             }
 
         }
+
+        private async void btnDescargaVideos_Click(object sender, RoutedEventArgs e)
+        {
+            // Aquí deberías tener la lógica para obtener el nombre del video que estás reproduciendo
+            string nombreVideo = txtBlockTitulo.Text;
+
+            // Llamamos al método de descarga
+            await DescargarVideo(nombreVideo);
+        }
+
+        // Método para descargar el video
+        // Método para descargar el video
+        private async Task DescargarVideo(string nombreVideo)
+        {
+            try
+            {
+                // Obtener la carpeta "Assets"
+                StorageFolder assetsFolder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("Assets");
+
+                // Obtener el archivo del video desde la carpeta "Assets"
+                StorageFile videoFile = await assetsFolder.GetFileAsync(nombreVideo + ".mp4");
+
+                // Configurar el cuadro de diálogo para la ubicación de descarga
+                FileSavePicker savePicker = new FileSavePicker();
+                savePicker.SuggestedStartLocation = PickerLocationId.Downloads;
+                savePicker.FileTypeChoices.Add("Video", new[] { ".mp4" });
+                savePicker.SuggestedFileName = nombreVideo;
+
+                // Mostrar el cuadro de diálogo y obtener la ubicación seleccionada por el usuario
+                StorageFile destino = await savePicker.PickSaveFileAsync();
+
+                // Copiar el archivo al destino seleccionado por el usuario
+                if (destino != null)
+                {
+                    await videoFile.CopyAndReplaceAsync(destino);
+                    MostrarMensaje("Descarga exitosa");
+                }
+            }
+            catch (Exception ex)
+            {
+                MostrarMensaje($"Error al descargar el video: {ex.Message}");
+            }
+        }
+
+        private async void MostrarMensaje(string mensaje)
+        {
+            var mensajeDialog = new Windows.UI.Popups.MessageDialog(mensaje, "Información");
+            await mensajeDialog.ShowAsync();
+        }
+
     }
 
 }
